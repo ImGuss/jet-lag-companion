6a24d2a4eb190f2ec3832bd01dc59adffd45771f
Gustavo Gutierrez <imguss@gmail.com>
2026-06-15 23:52:06 -0700

    wired up initial game state and state reducer

--- client/src/hooks/gameStateReducer.ts
+++ client/src/hooks/gameStateReducer.ts
@@ -5,3 +5,36 @@ import type {
   ActiveCountry
 } from '@shared/types'
 
+const initialState: GameState = {
+  activeCountry: null,
+  eliminatedRegions: [],
+  radiusMarkers: []
+}
+
+type GameAction =
+  | { type: 'SET_ACTIVE_COUNTRY'; payload: ActiveCountry }
+  | { type: 'ADD_RADIUS_MARKER'; payload: RadiusMarker }
+  | { type: 'REMOVE_RADIUS_MARKER'; payload: { id: string } }
+  | { type: 'RESET_GAME_STATE' }
+
+function gameStateReducer(state: GameState, action: GameAction): GameState {
+  switch (action.type) {
+    case 'SET_ACTIVE_COUNTRY':
+      return { ...state, activeCountry: action.payload, eliminatedRegions: [], radiusMarkers: [] }
+    case 'ADD_RADIUS_MARKER':
+      // TODO: Computer geo.ts
+      return { ...state, radiusMarkers: [...state.radiusMarkers, action.payload] }
+    case 'REMOVE_RADIUS_MARKER':
+      return {
+        ...state,
+        radiusMarkers: state.radiusMarkers.filter(marker => marker.id !== action.payload.id),
+        eliminatedRegions: state.eliminatedRegions.filter(region => region.sourceMarkerId !== action.payload.id)
+      }
+    case 'RESET_GAME_STATE':
+      return initialState
+    default:
+      return state
+  }
+}
+
+export { initialState, gameStateReducer }
