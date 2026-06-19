827b2dc9179b4bde2421f769e188903074cbe86e
Gustavo Gutierrez <imguss@gmail.com>
2026-06-18 22:25:13 -0700

    finished game state context and exported context hook

--- client/src/contexts/GameStateContext.tsx
+++ client/src/contexts/GameStateContext.tsx
@@ -1,3 +1,33 @@
+import { createContext, useContext } from 'react'
 import { useGameState } from '../hooks/useGameState'
 
+import type { ReactNode } from 'react'
+
 type GameStateContextType = ReturnType<typeof useGameState> | null
+
+const GameStateContext = createContext<GameStateContextType>(null)
+
+interface GameStateProviderProps {
+  children: ReactNode;
+}
+
+const GameStateProvider = (props: GameStateProviderProps) => {
+  const value = useGameState()
+
+  return (
+    <GameStateContext.Provider value={value}>
+      {props.children}
+    </GameStateContext.Provider>
+  )
+}
+
+export const useGameStateContext = () => {
+  const context =  useContext(GameStateContext)
+  if (context === null) {
+    throw new Error('useGameStateContext must be used within a GameStateProvider')
+  }
+
+  return context
+}
+
+export default GameStateProvider
