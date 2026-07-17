3a00e10d62ef6f3dfb371fbd62eb72354ce6d681
Gustavo Gutierrez <imguss@gmail.com>
2026-07-16 23:11:41 -0700

    return current state if no active geometry

--- client/src/hooks/gameStateReducer.ts
+++ client/src/hooks/gameStateReducer.ts
@@ -2,13 +2,18 @@ import {
   buildCircle,
   deriveActiveGeometry,
   validateRadiusMarker,
-  radiusElimination
+  validateElimArea,
+  radiusElimination,
+  buildBisectedPoly,
+  bisectorElimination
 } from '../lib/geo'
 
 import type {
   GameState,
   RadiusMarker,
-  ActiveCountry
+  BisectorMarker,
+  ActiveCountry,
+  EliminatedRegion
 } from '@shared/types'
 
 const initialState: GameState = {
@@ -16,6 +21,7 @@ const initialState: GameState = {
   activeGeometry: null,
   eliminatedRegions: [],
   radiusMarkers: [],
+  bisectorMarkers: [],
   isSelectingCountries: false
 }
 
@@ -24,6 +30,8 @@ type GameAction =
   | { type: 'REMOVE_ACTIVE_COUNTRY'; payload: { code: string } }
   | { type: 'ADD_RADIUS_MARKER'; payload: RadiusMarker }
   | { type: 'REMOVE_RADIUS_MARKER'; payload: { id: string } }
+  | { type: 'ADD_BISECTOR_MARKER'; payload: BisectorMarker }
+  | { type: 'REMOVE_BISECTOR_MARKER'; payload: { id: string } }
   | { type: 'SET_SELECTING_COUNTRIES'; payload: boolean }
   | { type: 'RESET_GAME_STATE' }
 
@@ -79,13 +87,14 @@ function gameStateReducer(state: GameState, action: GameAction): GameState {
         withinRadius: withinRadius
       })
 
-      const newRegion = {
+      const newRegion: EliminatedRegion = {
         id: crypto.randomUUID(),
         geometry: newRegionGeo,
+        sourceMarkerType: 'radius',
         sourceMarkerId: radiusMarkerId
       }
 
-      const newRadiusMarker = {
+      const newRadiusMarker: RadiusMarker = {
         id: radiusMarkerId,
         center: center,
         radius: radius,
@@ -105,10 +114,89 @@ function gameStateReducer(state: GameState, action: GameAction): GameState {
       }
     }
     case 'REMOVE_RADIUS_MARKER': {
+      const newEliminatedRegions = state.eliminatedRegions.filter(region => region.sourceMarkerId !== action.payload.id || region.sourceMarkerType !== 'radius')
+
+      const newActiveGeo = deriveActiveGeometry({
+        activeCountries: state.activeCountries,
+        eliminatedRegions: newEliminatedRegions
+      })
       return {
         ...state,
         radiusMarkers: state.radiusMarkers.filter(marker => marker.id !== action.payload.id),
-        eliminatedRegions: state.eliminatedRegions.filter(region => region.sourceMarkerId !== action.payload.id)
+        eliminatedRegions: newEliminatedRegions,
+        activeGeometry: newActiveGeo
+      }
+    }
+    case 'ADD_BISECTOR_MARKER': {
+      const { startPoint, endPoint, isCloserToEnd } = action.payload
+
+      if (!state.activeGeometry) {
+        // return some error
+        return state
+      }
+
+      const bisectorMarkerId = crypto.randomUUID()
+      const bisectedPoly = buildBisectedPoly({
+        startPoint: startPoint,
+        endPoint: endPoint,
+        isCloserToEnd: isCloserToEnd,
+        activeGeometry: state.activeGeometry
+      })
+
+      const isValid = validateElimArea({
+        elimFeature: bisectedPoly,
+        activeGeometry: state.activeGeometry
+      })
+
+      if (!isValid.ok) {
+        // return error
+        return state
+      }
+
+      const newRegionGeo = bisectorElimination({
+        elimFeature: bisectedPoly,
+        activeGeometry: state.activeGeometry
+      })
+
+      const newRegion: EliminatedRegion = {
+        id: crypto.randomUUID(),
+        geometry: newRegionGeo,
+        sourceMarkerType: 'bisector',
+        sourceMarkerId: bisectorMarkerId
+      }
+
+      const newBisectorMarker: BisectorMarker = {
+        id: bisectorMarkerId,
+        startPoint: startPoint,
+        endPoint: endPoint,
+        isCloserToEnd: isCloserToEnd
+      }
+
+      const newEliminatedRegions = [...state.eliminatedRegions, newRegion]
+      const newActiveGeo = deriveActiveGeometry({
+        activeCountries: state.activeCountries,
+        eliminatedRegions: newEliminatedRegions
+      })
+
+      return {
+        ...state,
+        activeGeometry: newActiveGeo,
+        eliminatedRegions: newEliminatedRegions,
+        bisectorMarkers: [...state.bisectorMarkers, newBisectorMarker]
+      }
+    }
+    case 'REMOVE_BISECTOR_MARKER': {
+      const newEliminatedRegions = state.eliminatedRegions.filter(region => region.sourceMarkerId !== action.payload.id || region.sourceMarkerType !== 'bisector')
+
+      const newActiveGeo = deriveActiveGeometry({
+        activeCountries: state.activeCountries,
+        eliminatedRegions: newEliminatedRegions
+      })
+      return {
+        ...state,
+        bisectorMarkers: state.bisectorMarkers.filter(marker => marker.id !== action.payload.id),
+        eliminatedRegions: newEliminatedRegions,
+        activeGeometry: newActiveGeo
       }
     }
     case 'SET_SELECTING_COUNTRIES': {
