96b7dc96aa669c7bfb0b1d3a302eb955fe04c4f2
Gustavo Gutierrez <imguss@gmail.com>
2026-07-21 20:55:48 -0700

    add radius preview state. will eventually go into a popup module too

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import GameStateProvider from './contexts/GameStateContext'
 
 import Map from './components/Map'
@@ -6,11 +8,23 @@ import BottomSheet from './components/BottomSheet'
 import './App.css'
 
 function App() {
+  const [center, setCenter] = useState<[number, number] | null>(null)
+  const [radius, setRadius] = useState<number | null>(null)
+  const [isPlacing, setIsPlacing] = useState(false)
 
   return (
     <GameStateProvider>
       <section>
-        <Map />
+        <Map
+          radiusPreview={{
+            center,
+            radius,
+            isPlacing,
+            setCenter,
+            setRadius,
+            setIsPlacing
+          }}
+        />
         <BottomSheet />
       </section>
     </GameStateProvider>
